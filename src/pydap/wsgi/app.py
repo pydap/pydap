a746ebef8e0c17e4c23f30aa7a2d33b791b61c62
Roberto De Almeida <roberto@dealmeida.net>
2013-06-14 19:38:36 -0700

    Working on the simple app

--- src/pydap/wsgi/app.py
+++ src/pydap/wsgi/app.py
@@ -17,92 +17,101 @@ Options:
 import os
 import re
 import mimetypes
-
-import pkg_resources
-from webob import Request, Response, exc
-from webob.static import DirectoryApp
-from webob.dec import wsgify
-import selector
+from datetime import datetime
 
 from jinja2 import Environment, PackageLoader, FileSystemLoader, ChoiceLoader
+from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import responder
+from werkzeug.exceptions import Unauthorized, NotFound
 
 from pydap.lib import __version__
 from pydap.handlers.lib import get_handler, load_handlers
+from pydap.exceptions import ExtensionNotSupportedError
 
 
-class DapServer(DirectoryApp):
+class DapServer(object):
 
     """A directory app that creates file listings and handle DAP requests."""
 
     def __init__(self, path, templates=None):
-        super(DapServer, self).__init__(path, hide_index_with_redirect=True)
+        self.path = os.path.abspath(path)
 
         # the default loader reads templates from the package
         loaders = [PackageLoader("pydap", "wsgi/templates")]
+
+        # optionally, the user can specify a template directory that will 
+        # override the default templates
         if templates is not None:
             loaders.insert(0, FileSystemLoader(templates))
+
+        # set the environment; this is also used by pydap responses that need
+        # to render templates (like HTML, WMS, KML, etc.)
         self.env = Environment(loader=ChoiceLoader(loaders))
-        self.env.filters["format_size"] = format_size
+        self.env.filters["formatsize"] = formatsize
+        self.env.filters["datetimeformat"] = datetimeformat
 
         # cache available handlers
         self.handlers = load_handlers()
 
-    @wsgify
-    def __call__(self, req):
-        """WSGI application callable.
+    @responder
+    def __call__(self, environ, start_response):
+        """WSGI application callable."""
+        request = Request(environ)
+        path = os.path.abspath(
+            os.path.join(self.path, *request.path.split('/')))
 
-        We first pass the request to the super class, and if it returns 404 we
-        strip the extension assuming it's a DAP request and check for an
-        existing and supported file.
-
-        """
-        res = super(DapServer, self).__call__(req)
-        if not isinstance(res, exc.HTTPNotFound):
-            return res
+        if not path.startswith(self.path):
+            return Unauthorized()
+        elif os.path.exists(path):
+            if os.path.isdir(path):
+                return self.index(path, request)
+            else:
+                return Response(open(path), direct_passthrough=True)
 
         # strip DAP extension (``.das``, eg) and see if the file exists
-        path = os.path.abspath(
-            os.path.join(self.path, *req.path_info.split('/')))
-        base, ext = os.path.splitext(path)
-
-        if not base.startswith(self.path):
-            return exc.HTTPForbidden()
-        elif os.path.isfile(base):
-            # pass the request to the proper handler
-            req.env['pydap.jinja2.environment'] = self.env
-            return req.get_response(get_handler(base, self.handlers))
+        path, ext = os.path.splitext(path)
+        if os.path.isfile(path):
+            request.environ['pydap.jinja2.environment'] = self.env
+            return get_handler(path, self.handlers)
         else:
-            # return the original 404
-            return res
+            return NotFound()
 
-    def index(self, req, path):
+    def index(self, directory, request):
         """Build a directory listing."""
-        directory, index = os.path.split(path)
         content = [
             os.path.join(directory, name) for name in os.listdir(directory)]
 
         files = [{
             "name": os.path.split(path)[1],
             "size": os.path.getsize(path),
-            "modified": os.path.getmtime(path),
+            "last_modified": datetime.fromtimestamp(os.path.getmtime(path)),
             "supported": supported(path, self.handlers),
-        } for name in content if os.path.isfile(path)]
-        files.sort(key=lambda d: alphanum_key(d['name']))
+        } for path in content if os.path.isfile(path)]
+        files.sort(key=lambda d: alphanum_key(d["name"]))
 
-        directories = [
-            os.path.split(path)[1] for path in content if os.path.isdir(path)]
-        directories.sort(key=alphanum_key)
+        directories = [{
+            "name": os.path.split(path)[1],
+            "last_modified": datetime.fromtimestamp(os.path.getmtime(path)),
+        } for path in content if os.path.isdir(path)]
+        directories.sort(key=lambda d: alphanum_key(d["name"]))
+
+        tokens = directory[len(self.path):].split('/')[1:]
+        breadcrumbs = [{
+            "url": request.url_root + '/'.join(tokens[:i+1]),
+            "title": token,
+        } for i, token in enumerate(tokens)]
 
         context = {
+            "root": request.url_root,
+            "breadcrumbs": breadcrumbs,
             "directories": directories,
             "files": files,
             "version": __version__,
         }
         template = self.env.get_template("index.html")
         return Response(
-            body=template.render(context),
-            content_type="text/html",
-            charset="utf-8")
+            response=template.render(context),
+            content_type="text/html; charset=utf-8")
 
 
 def supported(filepath, handlers=None):
@@ -137,7 +146,7 @@ def alphanum_key(s):
     return [tryint(c) for c in re.split('([0-9]+)', s)]
 
 
-def format_size(size):                                                          
+def formatsize(size):                                                          
     """Return file size as a human readable string."""                          
     if not size:
         return "Empty"                                                          
@@ -149,29 +158,9 @@ def format_size(size):
     return "%d PB" % size
 
 
-class ResourceStaticServer(object):
-
-    """Server for static files stored in Python package.
-    
-    This is used to load static assets from the package when no directory is
-    specified by the user."""
-
-    def __init__(self, package, resource_path):
-        self.package = package
-        self.resource_path = resource_path
-
-    @wsgify
-    def __call__(self, req):
-        resource = os.path.join(
-            self.resource_path, *req.path_info.split('/'))
-        if not pkg_resources.resource_exists(self.package, resource):
-            return exc.HTTPNotFound(req.path_info)
-
-        content_type, content_encoding = mimetypes.guess_type(resource)
-        return Response(
-            body=pkg_resources.resource_string(self.package, resource),
-            content_type=content_type,
-            content_encoding=content_encoding)
+def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
+    """Return a formatted datetime object."""
+    return value.strftime(format)
 
 
 def main():
@@ -179,25 +168,23 @@ def main():
 
     from docopt import docopt
     from gunicorn.app.pasterapp import paste_server
+    from werkzeug.wsgi import SharedDataMiddleware
 
     arguments = docopt(__doc__, version="Pydap %s" % __version__)
 
-    # create an app to serve static files from the templates
-    templates = arguments["--templates"]
+    # create pydap app
+    data, templates = arguments["--data"], arguments["--templates"]
+    app = DapServer(data, templates)
+    
+    # configure app so that is reads static assets from the template directory
+    # or from the package
     if templates and os.path.exists(os.path.join(templates, "static")):
-        static = DirectoryApp(os.path.join(templates, "static"))
+        static = os.path.join(templates, "static")
     else:
-        # load templates from the package
-        static = ResourceStaticServer("pydap", "wsgi/templates/static")
-
-    # set the pydap server
-    pydap = DapServer(arguments["--data"], templates)
-
-    # configure the WSGI app which will route requests
-    app = selector.Selector()
-    app.parser = lambda x: x
-    app.add("^\/static\/", GET=static)
-    app.add(".*", GET=pydap, POST=pydap)
+        static = ("pydap", "wsgi/templates/static")
+    app = SharedDataMiddleware(app, {
+        '/static': static,
+    })
 
     # configure WSGI server
     workers = multiprocessing.cpu_count() * 2 + 1
@@ -207,6 +194,7 @@ def main():
         port=int(arguments["--port"]),
         workers=workers,
         worker_class=arguments["--worker-class"])
+
 
 if __name__ == "__main__":
     main()
