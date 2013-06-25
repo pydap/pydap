b45244d11974f1c0b5b2561edb328ba52c24349e
Roberto De Almeida <roberto@dealmeida.net>
2013-06-25 08:25:09 -0700

    Add test case for variables with periods on their names

--- src/pydap/tests/test_special_chars.py
+++ src/pydap/tests/test_special_chars.py
@@ -6,6 +6,7 @@ else:
 
 import numpy as np
 from webtest import TestApp
+import requests
                                                                                 
 from pydap.model import *                                                       
 from pydap.handlers.lib import BaseHandler
@@ -16,35 +17,52 @@ from pydap.tests import requests_intercept
 class TestQuote(unittest.TestCase):                                            
     def setUp(self):
         # create dataset
-        dataset = DatasetType('test')
-        dataset['foo%5B'] = BaseType('foo[', np.array(1))
+        dataset = DatasetType("test")
+        dataset["foo%5B"] = BaseType("foo[", np.array(1))
 
         # create WSGI app
         self.app = TestApp(BaseHandler(dataset))
 
-    def test_children(self):
-        dataset = DatasetType('test')
-
-        #with self.assertRaises(KeyError):
-        #    dataset['foo['] = BaseType('foo[')
-
-        var = BaseType('foo[')
-        dataset[var.name] = var
-
     def test_dds(self):
-        self.assertEqual(self.app.get('/.dds').body,
+        self.assertEqual(self.app.get("/.dds").body,
             """Dataset {
     Int32 foo%5B;
 } test;
 """)
 
     def test_client(self):
-        import requests
         requests.old_get = requests.get
-        requests.get = requests_intercept(self.app, 'http://localhost:8001/')
+        requests.get = requests_intercept(self.app, "http://localhost:8001/")
 
-        dataset = open_url('http://localhost:8001/')
-        self.assertEqual(dataset['foo%5B'].name, 'foo%5B')
-        self.assertEqual(dataset['foo%5B'][0], 1)
+        dataset = open_url("http://localhost:8001/")
+        self.assertEqual(dataset["foo%5B"].name, "foo%5B")
+        self.assertEqual(dataset["foo%5B"][0], 1)
+
+        requests.get = requests.old_get
+
+
+class TestPeriod(unittest.TestCase):
+    def setUp(self):
+        # create dataset
+        dataset = DatasetType("test")
+        dataset["a.b"] = BaseType("a.b", np.array(1))
+
+        # create WSGI app
+        self.app = TestApp(BaseHandler(dataset))
+
+    def test_dds(self):
+        self.assertEqual(self.app.get("/.dds").body,
+            """Dataset {
+    Int32 a%2Eb;
+} test;
+""")
+
+    def test_client(self):
+        requests.old_get = requests.get
+        requests.get = requests_intercept(self.app, "http://localhost:8001/")
+
+        dataset = open_url("http://localhost:8001/")
+        self.assertEqual(dataset["a.b"].name, "a%2Eb")
+        self.assertEqual(dataset["a.b"][0], 1)
 
         requests.get = requests.old_get
