d1656e10379ef8b36d94ff57a8dac83781294a8e
Roberto De Almeida <roberto@dealmeida.net>
2013-07-24 09:45:11 -0700

    Tests for server-side functions

--- src/pydap/wsgi/ssf.py
+++ src/pydap/wsgi/ssf.py
@@ -1,3 +1,11 @@
+"""An implementation of server-side functions.
+
+Pydap implements DAP server-side functions throught a custom WSGI middleware.
+This simplifies writing custom handlers, since they don't need to parse and
+apply the function calls themselves.
+
+"""
+
 import re
 import operator
 import ast
@@ -6,60 +14,75 @@ from webob import Request
 from pkg_resources import iter_entry_points
 import numpy as np
 
-from pydap.model import *
+from pydap.model import DatasetType, SequenceType
 from pydap.parsers import parse_ce
 from pydap.lib import walk, fix_shorthand
 from pydap.handlers.lib import BaseHandler, apply_projection
+from pydap.exceptions import ServerError
 
 
-FUNCTION = re.compile(r'([^(]*)\((.*)\)') 
-RELOP = re.compile(r'(<=|<|>=|>|=|!=)')
+FUNCTION = re.compile(r'([^(]*)\((.*)\)')
+RELOP = re.compile(r'(<=|<|>=|>|=~|=|!=)')
 
 
 class ServerSideFunctions(object):
 
-    functions = dict((r.name, r.load())
-            for r in iter_entry_points('pydap.function'))
+    """A WebOb based middleware for handling server-side function calls.
 
-    def __init__(self, app):
+    The middleware works by removing function calls from the request,
+    forwarding the request to Pydap, and then applying the functions calls to
+    the returned dataset.
+
+    """
+
+    def __init__(self, app, **kwargs):
         self.app = app
+
+        self.functions = {}
+        for r in iter_entry_points("pydap.function"):
+            self.functions[r.name] = r.load()
+        self.functions.update(kwargs)
 
     def __call__(self, environ, start_response):
         # specify that we want the parsed dataset
         environ['x-wsgiorg.want_parsed_response'] = True
         req = Request(environ)
-        original_query = req.query_string
         projection, selection = parse_ce(req.query_string)
 
-        # apply selection without any function calls
-        req.query_string = '&'.join(expr for expr in selection if not FUNCTION.match(expr))
-        res = req.get_response(self.app)
+        # check if there are any functions calls in the request
+        called = (
+            any(s for s in selection if FUNCTION.match(s)) or
+            any(p for p in projection if isinstance(p, basestring)))
 
-        # ignore DAS requests
+        # ignore DAS requests and requests without functions
         path, response = req.path.rsplit('.', 1)
-        if response == 'das':
+        if response == 'das' or not called:
             return self.app(environ, start_response)
+
+        # apply selection without any function calls
+        req.query_string = '&'.join(
+            s for s in selection if not FUNCTION.match(s))
+        res = req.get_response(self.app)
 
         # get the dataset
         method = getattr(res.app_iter, 'x_wsgiorg_parsed_response', False)
         if not method:
-            environ['QUERY_STRING'] = original_query
-            return self.app(environ, start_response)
+            raise ServerError("Unable to call server-side function!")
         dataset = method(DatasetType)
 
         # apply selection containing server-side functions
-        selection = (expr for expr in selection if FUNCTION.match(expr))
+        selection = (s for s in selection if FUNCTION.match(s))
         for expr in selection:
             if RELOP.search(expr):
                 call, op, other = RELOP.split(expr)
                 op = {
-                        '<' : operator.lt,
-                        '>' : operator.gt,
-                        '!=': operator.ne,
-                        '=' : operator.eq,
-                        '>=': operator.ge,
-                        '<=': operator.le,
-                        '=~': lambda a, b: re.match(b, a),
+                    '<':  operator.lt,
+                    '>':  operator.gt,
+                    '!=': operator.ne,
+                    '=':  operator.eq,
+                    '>=': operator.ge,
+                    '<=': operator.le,
+                    '=~': lambda a, b: re.match(b, a),
                 }[op]
                 other = ast.literal_eval(other)
             else:
@@ -73,22 +96,26 @@ class ServerSideFunctions(object):
                 if sequence is var:
                     break
             else:
-                # get the data from the resulting variable, and use it to 
+                # get the data from the resulting variable, and use it to
                 # constrain the original dataset
                 child = list(sequence.children())[0]
                 data = np.fromiter(child.data, child.dtype)
-                valid = op(data, other)
+                if data.dtype.char == "S":
+                    valid = np.array(
+                        map(lambda v: op(str(v), str(other)), data), bool)
+                else:
+                    valid = op(data, other)
 
                 for sequence in walk(dataset, SequenceType):
                     sequence.data = np.rec.fromrecords(
-                            [tuple(row) for row in sequence],
-                            names=sequence.keys())[valid]
+                        [tuple(row) for row in sequence],
+                        names=sequence.keys())[valid]
 
         # now apply projection
         if projection:
             projection = fix_shorthand(projection, dataset)
-            base = [p for p in projection if not isinstance(p, basestring)] 
-            func = [p for p in projection if isinstance(p, basestring)] 
+            base = [p for p in projection if not isinstance(p, basestring)]
+            func = [p for p in projection if isinstance(p, basestring)]
 
             # apply non-function projection
             out = apply_projection(base, dataset)
@@ -97,7 +124,8 @@ class ServerSideFunctions(object):
             for call in func:
                 var = eval_function(dataset, call, self.functions)
                 for child in walk(var):
-                    parent = reduce(operator.getitem, [out] + child.id.split('.')[:-1])
+                    parent = reduce(
+                        operator.getitem, [out] + child.id.split('.')[:-1])
                     if child.name not in parent.keys():
                         parent[child.name] = child
                         break
@@ -111,6 +139,12 @@ class ServerSideFunctions(object):
 
 
 def eval_function(dataset, function, functions):
+    """Evaluate a given function on a dataset.
+
+    This function parses and evaluates a (possibly nested) function call,
+    returning its result.
+
+    """
     name, args = FUNCTION.match(function).groups()
 
     def tokenize(input):
