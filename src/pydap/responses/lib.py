b2dab945ff7dbbd1f4567c09ee088253ae84fe6c
Roberto De Almeida <roberto@dealmeida.net>
2013-07-25 03:22:12 -0700

    Unit tests for `pydap.responses.lib`

--- src/pydap/responses/lib.py
+++ src/pydap/responses/lib.py
@@ -1,18 +1,55 @@
+"""Fundamental functions for Pydap responses.
+
+Pydap responses are WSGI applications that convert a dataset into different
+representations, like the DDS, DAS and DODS responses described in the DAP
+specification.
+
+In addition to the official responses, Pydap also has responses that generate
+KML, WMS, JSON, etc., installed as third-party Python packages that declare the
+"pydap.response" entry point.
+
+"""
+
 from pkg_resources import iter_entry_points
 
-from pydap.model import *
+from pydap.model import DatasetType
 from pydap.lib import __version__
 
 
 def load_responses():
-    return dict((r.name, r.load()) for r in iter_entry_points('pydap.response'))
+    """Load all available responses from the system, returning a dictionary."""
+    return dict(
+        (r.name, r.load()) for r in iter_entry_points('pydap.response'))
 
 
 class BaseResponse(object):
+
+    """A base class for Pydap responses.
+
+    A Pydap response is a WSGI application that converts a dataset into any
+    other representation. The most know responses are the DDS, DAS and DODS
+    responses from the DAP spec, which describe the dataset structure,
+    attributes and data, respectively.
+
+    According to the WSGI specification, WSGI applications must returned an
+    iterable object when called. While this is traditionally a list of strings
+    representing an HTML response, this is not the case for Pydap. Pydap will
+    return an object (the response instance itself), which is an iterable that
+    yields the corresponding output (a DDS response, eg).
+
+    In practice, this means that the generation of the response is delayed
+    until the data is being sent to the client. But since the response object
+    also carries the original dataset, this means it's possible to write WSGI
+    middleware that modifies the dataset directly. A WSGI middleware can add
+    additional metadata to a dataset, eg, by adding attributes directly to the
+    dataset object, without having to generate a new response.
+
+    """
+
     def __init__(self, dataset):
         self.dataset = dataset
         self.headers = [
-                ('XDODS-Server', 'pydap/%s' % __version__),
+            ('XDODS-Server', 'pydap/%s' % __version__),
         ]
 
     def __call__(self, environ, start_response):
@@ -20,6 +57,14 @@ class BaseResponse(object):
         return self
 
     def x_wsgiorg_parsed_response(self, type):
+        """Avoid serialization of datasets.
+
+        This function will return the contained dataset if ``type`` is a
+        ``pydap.model.DatasetType`` object. Based on this proposal:
+
+            http://wsgi.readthedocs.org/en/latest/specifications/avoiding_serialization.html
+
+        """
         if type is DatasetType:
             return self.dataset
 
@@ -28,4 +73,5 @@ class BaseResponse(object):
             'Subclasses must implement __iter__')
 
     def close(self):
+        """Optional close method for the dataset."""
         pass
