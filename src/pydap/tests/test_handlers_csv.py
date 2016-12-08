4fefcc102b538c6252491cef6e3ed486002fad0f
Frederic Laliberte <laliberte.frederic@gmail.com>
2016-12-08 16:46:44 -0500

    Split CSVHandler test into a server-based and a non-server based version

--- src/pydap/tests/test_handlers_csv.py
+++ src/pydap/tests/test_handlers_csv.py
@@ -3,42 +3,17 @@
 import sys
 import csv
 import tempfile
-import multiprocessing
 import os
-import time
 import numpy as np
 
 from pydap.handlers.csv import CSVHandler
-from werkzeug.serving import run_simple
-from webob.request import Request
-from pydap.wsgi.ssf import ServerSideFunctions
-from pydap.client import open_url
+from pydap.handlers.dap import DAPHandler
 
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
 else:
     import unittest
-
-
-def run_simple_server(test_file):
-    application = CSVHandler(test_file)
-    application = ServerSideFunctions(application)
-    run_simple('localhost', 8001,
-               lambda x, y: application(check_for_shutdown(x), y),
-               use_reloader=True)
-
-
-def check_for_shutdown(environ):
-    if environ['PATH_INFO'] == '/shutdown':
-        shutdown_server(environ)
-    return environ
-
-
-def shutdown_server(environ):
-    if 'werkzeug.server.shutdown' not in environ:
-        raise RuntimeError('Not running the development server')
-    environ['werkzeug.server.shutdown']()
 
 
 class TestCSVHandler(unittest.TestCase):
@@ -50,7 +25,7 @@ class TestCSVHandler(unittest.TestCase):
             (13, 12.1, 'Kodiak_Trail')]
 
     def setUp(self):
-        """Create WSGI apps"""
+        """Create TEST data in CSV format"""
 
         # Create tempfile:
         fileno, self.test_file = tempfile.mkstemp(suffix='.csv')
@@ -62,17 +37,10 @@ class TestCSVHandler(unittest.TestCase):
             for row in self.data:
                 writer.writerow(row)
 
-        # Start a simple WSGI server:
-        self.server_process = multiprocessing.Process(target=run_simple_server,
-                                                      args=(self.test_file,))
-        self.server_process.start()
-        # Wait a little while for the server to start:
-        time.sleep(3)
-
     def test_open(self):
         """Test that dataset has the correct data proxies for grids."""
-        url = "http://localhost:8001/" + os.path.basename(self.test_file)
-        dataset = open_url(url)
+        dataset = DAPHandler("http://localhost:8001/",
+                             CSVHandler(self.test_file)).dataset
         seq = dataset['sequence']
         dtype = [('index', '<i4'),
                  ('temperature', '<f8'),
@@ -83,12 +51,5 @@ class TestCSVHandler(unittest.TestCase):
                                       np.array(self.data, dtype=dtype))
 
     def tearDown(self):
-        # Shutdown the server:
-        (Request
-         .blank("http://localhost:8001/shutdown")
-         .get_response())
-        self.server_process.terminate()
-        self.server_process.join()
-        del(self.server_process)
         # Remove test file:
         os.remove(self.test_file)
