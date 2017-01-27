a1d260cd66dbb423e8a17eb21354a17125d74eef
Frederic Laliberte <laliberte.frederic@gmail.com>
2017-01-27 18:49:21 -0500

    Some more clean-up of the follow_redirects code. Addition of a exception catch in case of testing.

--- src/pydap/net.py
+++ src/pydap/net.py
@@ -1,6 +1,7 @@
 from webob.request import Request
 from webob.exc import HTTPError
 from contextlib import closing
+from requests.exceptions import MissingSchema
 
 from six.moves.urllib.parse import urlsplit, urlunsplit
 
@@ -30,8 +31,7 @@ def raise_for_status(response):
         )
 
 
-def follow_redirect(url, application=None, session=None,
-                    cookies_dict=None):
+def follow_redirect(url, application=None, session=None):
     """
     This function essentially performs the following command:
     >>> Request.blank(url).get_response(application)
@@ -40,23 +40,30 @@ def follow_redirect(url, application=None, session=None,
     headers as the passed session.
     """
 
-    if session is not None:
-        # Use session to follow redirects:
-        with closing(session.head(url)) as head:
-            req = Request.blank(head.url)
+    req = create_request(url, session=session)
+    return req.get_response(application)
 
-            # Get cookies from head:
-            if cookies_dict is None:
+
+def create_request(url, session=None):
+    if session is not None:
+        try:
+            # Use session to follow redirects:
+            with closing(session.head(url)) as head:
+                req = Request.blank(head.url)
+
+                # Get cookies from head:
                 cookies_dict = head.cookies.get_dict()
 
-            # Set request cookies to the head cookies:
-            req.headers['Cookie'] = ','.join(name + '=' + cookies_dict[name]
-                                             for name in cookies_dict)
-            # Set the headers to the session headers:
-            for item in head.request.headers:
-                req.headers[item] = head.request.headers[item]
-    else:
-        # Use session to follow redirects:
-        req = Request.blank(url)
-
-    return req.get_response(application)
+                # Set request cookies to the head cookies:
+                req.headers['Cookie'] = ','.join(name + '=' +
+                                                 cookies_dict[name]
+                                                 for name in cookies_dict)
+                # Set the headers to the session headers:
+                for item in head.request.headers:
+                    req.headers[item] = head.request.headers[item]
+                return req
+        except MissingSchema:
+            # Missing schema can occur in tests when the url
+            # is not pointing to any resource. Simply pass.
+            pass
+    return Request.blank(url)
