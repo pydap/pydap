3b5f7a8e37b2e76543fd10dfa1fe82aa3830e550
Frédéric Laliberté <laliberte.frederic@gmail.com>
2017-04-17 20:14:01 -0400

    Unify dods parsers (#91)
    Consolidated DODS data conversion into one function.

--- src/pydap/lib.py
+++ src/pydap/lib.py
@@ -199,3 +199,36 @@ def load_from_entry_point_relative(r, package):
     except ImportError:
         # This is only used in handlers testing:
         return r.name, r.load()
+
+
+class StreamReader(object):
+
+    """Class to allow reading a `urllib3.HTTPResponse`."""
+
+    def __init__(self, stream):
+        self.stream = stream
+        self.buf = bytearray()
+
+    def read(self, n):
+        """Read and return `n` bytes."""
+        while len(self.buf) < n:
+            bytes_read = next(self.stream)
+            self.buf.extend(bytes_read)
+
+        out = bytes(self.buf[:n])
+        self.buf = self.buf[n:]
+        return out
+
+
+class BytesReader(object):
+
+    """Class to allow reading a `bytes` object."""
+
+    def __init__(self, data):
+        self.data = data
+
+    def read(self, n):
+        """Read and return `n` bytes."""
+        out = self.data[:n]
+        self.data = self.data[n:]
+        return out
