2675dfe7c7ab1f59fcd667624651eb2e1e722146
Tom Kralidis <tomkralidis@gmail.com>
2017-09-18 21:46:17 -0400

    test fix

--- src/pydap/__init__.py
+++ src/pydap/__init__.py
@@ -2,6 +2,6 @@
 Declare the namespace ``pydap`` here.
 '''
 
-__version__ = '3.2.2'
+__version__ = __import__('pkg_resources').get_distribution(__name__).version
 
 __import__('pkg_resources').declare_namespace(__name__)
