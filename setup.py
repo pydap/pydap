b6e10a0fd6f4dec0843d8044946420046ce56bd5
Tom Kralidis <tomkralidis@gmail.com>
2018-10-27 13:19:06 -0400

    drop Python 3.4, add 3.7 (#175)

--- setup.py
+++ setup.py
@@ -88,9 +88,9 @@ setup(name='pydap',
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6"
+            "Programming Language :: Python :: 3.7"
             # Get strings from
             # http://pypi.python.org/pypi?%3Aaction=list_classifiers
             ],
