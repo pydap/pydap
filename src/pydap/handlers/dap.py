584c7aa01eea83362de1b96864fae74cecc74bc9
Niklas Griessbaum <griessbaum@gmail.com>
2022-07-07 11:09:01 -0700

    oops

--- src/pydap/handlers/dap.py
+++ src/pydap/handlers/dap.py
@@ -202,7 +202,6 @@ class BaseProxy(object):
 
         # download and unpack data
         logger.info("Fetching URL: %s" % url)
-        print(url)
         r = GET(url, self.application, self.session, timeout=self.timeout, verify=self.verify)
         raise_for_status(r)
         dds, data = safe_dds_and_data(r, self.user_charset)
