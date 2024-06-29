3667e4d96caca10ed13f01bcd812a53ea8fe5fa3
Miguel A. Jimenez-Urias <mjimenez@opendap.org>
2024-06-29 00:23:20 -0700

    response for data with Groups

--- src/pydap/responses/dmr.py
+++ src/pydap/responses/dmr.py
@@ -12,11 +12,12 @@ try:
 except ImportError:
     from singledispatch import singledispatch
 
-from ..lib import DAP4_TO_NUMPY_PARSER_TYPEMAP, __version__
+from ..lib import __version__
 from ..model import BaseType, DatasetType, GroupType, SequenceType, StructureType
 from .lib import BaseResponse
 
 INDENT = " " * 4
+namespace = {"": "http://xml.opendap.org/ns/DAP/4.0#"}
 
 
 class DMRResponse(BaseResponse):
@@ -34,13 +35,10 @@ class DMRResponse(BaseResponse):
         )
 
     def __iter__(self):
-        # generate DDS
+        # generate DMR
+        yield '<?xml version="1.0" encoding="ISO-8859-1"?>\n'.encode("ascii")
         for line in dmr(self.dataset):
             yield line.encode("ascii")
-
-        yield b"Data:\n"
-        for block in dmr(self.dataset):
-            yield block
 
 
 @singledispatch
@@ -49,54 +47,67 @@ def dmr(var):
     raise StopIteration
 
 
+@dmr.register(DatasetType)
+def _(var, level=0, path="/"):
+    str0 = 'Dataset xmlns="{namespace}"'.format(namespace=namespace[""])
+    str1 = ' xml:base="{url}"'.format(url="http://localhost:8001")
+    str2 = ' dapVersion="4.0" dmrVersion="1.0"'
+    str3 = ' name="{name}">\n'.format(name=var.name)
+    yield "<{indent}".format(indent=level * INDENT) + str0 + str1 + str2 + str3
+    for child in var.children():
+        for line in dmr(child, level + 1, path):
+            yield line
+    yield "{indent}</Dataset>\n".format(indent=level * INDENT)
+
+
 @dmr.register(StructureType)
 def _structuretype(var):
-    for child in var.children():
-        for block in dmr(child):
-            yield block
-
-
-@dmr.register(DatasetType)
-def _(var, level=0, sequence=0):
-    yield "{indent}Dataset {{\n".format(indent=level * INDENT)
-    for child in var.children():
-        for line in dmr(child, level + 1, sequence):
-            yield line
-    yield "{indent}}} {name};\n".format(indent=level * INDENT, name=var.name)
+    pass
 
 
 @dmr.register(SequenceType)
 def _sequencetype(var, level=0, sequence=0):
-    yield "{indent}Sequence {{\n".format(indent=level * INDENT)
-    for child in var.children():
-        for line in dmr(child, level + 1, sequence + 1):
-            yield line
-    yield "{indent}}} {name};\n".format(indent=level * INDENT, name=var.name)
+    pass
 
 
 @dmr.register(GroupType)
-def _grouptype(var, level=0, sequence=0):
-    yield "{indent}Group {{\n".format(indent=level * INDENT)
+def _grouptype(var, level=0, path="/"):
+    dims = var.dimensions
+    yield '{indent}<Group name="{name}">\n'.format(indent=level * INDENT, name=var.name)
+    path += var.name + "/"
+    for dim in dims:
+        size = var[dim].shape[0]
+        yield '{indent}<Dimension name="{name}" size="{size}"/>\n'.format(
+            indent=(level + 1) * INDENT, name=dim, size=size
+        )
     for child in var.children():
-        for line in dmr(child, level + 1, sequence):
+        for line in dmr(child, level + 1, path):
             yield line
-    yield "{indent}}} {name};\n".format(indent=level * INDENT, name=var.name)
+    yield "{indent}</Group>\n".format(indent=level * INDENT)
 
 
 @dmr.register(BaseType)
-def _basetype(var, level=0, sequence=0):
-    shape = var.shape[sequence:]
-
-    if var.dimensions:
-        shape = "".join(map("[{0[0]} = {0[1]}]".format, zip(var.dimensions, shape)))
-    elif len(shape) == 1:
-        shape = "[{0} = {1}]".format(var.name, shape[0])
-    else:
-        shape = "".join("[{0}]".format(len) for len in shape)
-
-    yield "{indent}{type} {name}{shape};\n".format(
+def _basetype(var, level=0, path="/"):
+    _ntype = var.dtype
+    _vartype = str(_ntype)[0].upper() + str(_ntype)[1:]
+    yield '{indent}<{type} name="{name}">\n'.format(
         indent=level * INDENT,
-        type=DAP4_TO_NUMPY_PARSER_TYPEMAP[var.dtype],
+        type=_vartype,
         name=var.name,
-        shape=shape,
     )
+    # get dimensions
+    dims = var.dimensions
+    for dim in dims:
+        yield '{indent}<Dim name="{name}"/>\n'.format(
+            indent=(level + 1) * INDENT, name=path + dim
+        )
+    for key, value in var.attributes.items():
+        _type = type(value).__name__
+        yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
+            indent=(level + 1) * INDENT, name=key, type=_type
+        )
+        yield "{indent}<Value>{val}</Value>".format(
+            indent=(level + 2) * INDENT, val=value
+        )
+        yield "{indent}</Attribute>\n".format(indent=(level + 1) * INDENT)
+    yield "{indent}</{type}>\n".format(indent=level * INDENT, type=_vartype)
