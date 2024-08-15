f516d72f092cc71d60d384bde171641a8e44d7a6
Miguel Jimenez <mjimenez@opendap.org>
2024-08-15 16:27:05 -0700

    correctly define `named dimensions` at `root` level (#348)
    * leave `named Dimensions` w/o assigned variable
    
    * set `Global dimensions` at top of xml document
    
    * update testing
    
    * define `Global dimensions` at `root` level
    
    * add testing for global dimensions and named dimensions like `nv`
    
    * remove `skip` flag -- no longer needed

--- src/pydap/parsers/dmr.py
+++ src/pydap/parsers/dmr.py
@@ -154,9 +154,6 @@ def get_maps(element):
 def dmr_to_dataset(dmr):
     """Return a dataset object from a DMR representation."""
 
-    # this should simple be:
-    # return DMRParser(dmr).parse()
-
     # Parse the DMR. First dropping the namespace
     dom_et = DMRParser(dmr).node
     # emtpy dataset
@@ -164,6 +161,14 @@ def dmr_to_dataset(dmr):
 
     variables = get_variables(dom_et)
     named_dimensions = get_named_dimensions(dom_et)
+
+    # get Global dimensions at root level
+    global_dimensions = []
+    for name, size in named_dimensions.items():
+        if len(name.split("/")) == 1:
+            global_dimensions.append([name, size])
+
+    dataset.dimensions = tuple(tuple(item) for item in global_dimensions)
 
     # Add size entry for dimension variables
     for name, size in named_dimensions.items():
@@ -179,25 +184,13 @@ def dmr_to_dataset(dmr):
         variable["maps"] = get_maps(variable["element"])
         variable["shape"] = get_dim_sizes(variable["element"])
 
-    # Add size entry for dimension variables
-    for name, size in named_dimensions.items():
-        if name not in variables:
-            # We might have dimensions that only have a declaration,
-            # so we need to add them to the variables
-            variables[name] = {
-                "name": name,
-                "size": size,
-                "dims": [name],
-                "dtype": "int",
-                "maps": (),
-                "attributes": {},
-                "shape": (),
-            }
-
     # Add shape element to variables
     for name, variable in variables.items():
         for dim in variable["dims"]:
-            variable["shape"] += (variables[dim]["size"],)
+            if dim in variables.items():
+                variable["shape"] += (variables[dim]["size"],)
+            else:
+                variable["shape"] += (named_dimensions[dim],)
 
     for name, variable in variables.items():
         var_name = variable["name"]
