4a7b39bf555b20c6619275e9ecd36f36a37d1076
Miguel A. Jimenez-Urias <mjimenez@opendap.org>
2024-08-23 11:37:43 -0700

    Update documentation

--- build/lib/pydap/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("pydap")
-except PackageNotFoundError:
-    # package is not installed
-    pass
--- build/lib/pydap/tests/test_version.py
+++
@@ -1,5 +0,0 @@
-import pydap
-
-
-def test_version() -> None:
-    assert pydap.__version__ != "999"
