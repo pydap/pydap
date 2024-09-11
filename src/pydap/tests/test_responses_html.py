0e54368e0309e293f4a8f8125160102ce5ec3947
Miguel A. Jimenez-Urias <mjimenez@opendap.org>
2024-09-11 13:29:23 -0700

    update the added SimpleGroup html response

--- src/pydap/tests/test_responses_html.py
+++ src/pydap/tests/test_responses_html.py
@@ -180,7 +180,7 @@ class TestHTMLResponseSimpleGroup(unittest.TestCase):
                     ("OPeNDAP-Server", "pydap/" + __version__),
                     ("Content-description", "DAP_form"),
                     ("Content-type", "text/html; charset=utf-8"),
-                    ("Content-Length", "8348"),
+                    ("Content-Length", "8368"),
                 ]
             ),
         )
