6001d32dca12864e242e1408f36370d014ee7a1d
Mikejmnez <mjimenez@opendap.org>
2024-09-24 15:55:00 -0700

    add some testing

--- src/pydap/tests/datasets.py
+++ src/pydap/tests/datasets.py
@@ -222,7 +222,11 @@ SimpleGroup = DatasetType(
     description="A simple group for testing.",
     dimensions={"time": 1, "nv": 2},
 )
-SimpleGroup.createGroup("SimpleGroup", dimensions={"Y": 4, "X": 4})
+SimpleGroup.createGroup(
+    "SimpleGroup",
+    dimensions={"Y": 4, "X": 4},
+    Description="Test group with numerical data",
+)
 SimpleGroup.createVariable(
     name="/SimpleGroup/Temperature",
     data=np.arange(10, 26, 1, dtype="f4").reshape(1, 4, 4),
--- src/pydap/tests/test_responses_html.py
+++ src/pydap/tests/test_responses_html.py
@@ -180,7 +180,7 @@ class TestHTMLResponseSimpleGroup(unittest.TestCase):
                     ("OPeNDAP-Server", "pydap/" + __version__),
                     ("Content-description", "DAP_form"),
                     ("Content-type", "text/html; charset=utf-8"),
-                    ("Content-Length", "8416"),
+                    ("Content-Length", "8500"),
                 ]
             ),
         )
