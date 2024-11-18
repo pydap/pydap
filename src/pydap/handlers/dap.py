2a944de6076ae90f43fb191fd35d204c72967d97
Mikejmnez <mjimenez@opendap.org>
2024-11-18 09:02:31 -0800

    improve User warning message

--- src/pydap/handlers/dap.py
+++ src/pydap/handlers/dap.py
@@ -338,8 +338,9 @@ def safe_dmr_and_data(r, user_charset, url):
     if "thredds" in url.split("/") or "dap4" in url.split("/"):
         data = data[2:]  # TDS
         warnings.warn(
-            "Full DAP4 support for TDS data servers remains under development."
-            "We recommend to continue to use DAP2 protocol. See pydap/issues#401",
+            "Full DAP4 support for TDS data servers remains under development"
+            " for pydap. We recommend to continue to use DAP2 protocol instead."
+            " See: https://github.com/pydap/pydap/issues/401",
             UserWarning,
         )
     else:
