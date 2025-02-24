4ae80ec5c9f7a6e10f7afd214f90a82232f38f90
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-02-24 14:54:48 -0800

    update testing using requests library, and GET method

--- src/pydap/tests/test_net.py
+++ src/pydap/tests/test_net.py
@@ -4,7 +4,6 @@ Test the follow redirects and handling of more complex routing situations
 
 import requests
 import requests_mock
-from webob.request import Request
 
 from pydap.net import create_request
 
@@ -15,40 +14,48 @@ def test_redirect():
     # mock_url is redirected to https:
     redirect_url = "http://www.test2.com/"
     with requests_mock.Mocker() as m:
-        m.register_uri("HEAD", mock_url, text="resp2", status_code=200)
+        m.register_uri("GET", mock_url, status_code=302, 
+        headers={"Location": redirect_url})
+        m.register_uri("GET", redirect_url, text="Final Destination", status_code=200)
         req = create_request(mock_url)
-        assert len(m.request_history) == 1
-        assert isinstance(req, Request)
-        assert req.headers["Host"] == "www.test.com:80"
+        assert req.url == redirect_url
+        assert req.status_code == 200
+        assert req.text == "Final Destination"
+        assert isinstance(req, requests.Response)
 
     # Without session:
     with requests_mock.Mocker() as m:
         m.register_uri(
-            "HEAD",
+            "GET",
             mock_url,
             text="resp1",
             status_code=301,
             headers={"Location": redirect_url},
         )
-        m.register_uri("HEAD", redirect_url, text="resp2", status_code=200)
+        m.register_uri("GET", redirect_url, text="resp2", status_code=200)
         req = create_request(mock_url)
+        assert isinstance(req, requests.Response)
         # Ensure follow redirect:
         assert len(m.request_history) == 2
-        assert isinstance(req, Request)
-        assert req.headers["Host"] == "www.test2.com:80"
+        assert req.url == redirect_url
+        assert req.status_code == 200
+        assert req.text == "resp2"
+
 
     # With session:
     with requests_mock.Mocker() as m:
         m.register_uri(
-            "HEAD",
+            "GET",
             mock_url,
             text="resp1",
             status_code=301,
             headers={"Location": redirect_url},
         )
-        m.register_uri("HEAD", redirect_url, text="resp2", status_code=200)
+        m.register_uri("GET", redirect_url, text="resp2", status_code=200)
         req = create_request(mock_url, session=requests.Session())
         # Ensure follow redirect:
         assert len(m.request_history) == 2
-        assert isinstance(req, Request)
-        assert req.headers["Host"] == "www.test2.com:80"
+        assert isinstance(req, requests.Response)
+        assert req.url == redirect_url
+        assert req.status_code == 200
+        assert req.text == "resp2"
