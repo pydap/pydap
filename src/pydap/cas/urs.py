c43860ad5a5708f36477594e9d7999b61bf6a768
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-03-02 11:51:09 -0800

    adds deprecation warning cas.urs.setup_session. Suggests intead to use .netrc file

--- src/pydap/cas/urs.py
+++ src/pydap/cas/urs.py
@@ -1,33 +1,19 @@
-from . import get_cookies
+from warnings import warn
 
 
 def setup_session(username, password, check_url=None, session=None, verify=True):
     """
-    A special call to get_cookies.setup_session that is tailored for
-    URS EARTHDATA at NASA credentials.
-
-    Parameters:
-    ----------
-    username: str
-    password: str
-    check_url: str, None (default)
-    session: `requests.session`, None (default)
-    verify: bool, True (default)
-
-    Example Data Access and Authentification in:
-    see `https://opendap.github.io/documentation/tutorials/`
-
+    This function is deprecated and will be removed in version 1.0.0.
+    Please use new_function instead.
     """
-
-    if session is not None:
-        # URS connections cannot be kept alive at the moment.
-        session.headers.update({"Connection": "close"})
-    session = get_cookies.setup_session(
-        "https://urs.earthdata.nasa.gov",
-        username=username,
-        password=password,
-        session=session,
-        check_url=check_url,
-        verify=verify,
+    warn(
+        "`urs.setup_session` is deprecated and will be removed in the next release. "
+        "Instead, use a `.netrc` file instead for your EDL authentication credentials."
+        "With the `.netrc file`, authentication with URS EARTHDATA at NASA credentials"
+        "is now handled automatically by requests.session object. "
+        "See: \n"
+        "https://opendap.github.io/documentation/tutorials/ClientAuthentication.html",
+        DeprecationWarning,
+        stacklevel=2,
     )
-    return session
+    pass
