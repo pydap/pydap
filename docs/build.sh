ef74de03659932e8d165e35b1361c25a0ae1af01
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-03-26 22:54:55 -0700

    update auth in notebooks

--- docs/build.sh
+++ docs/build.sh
@@ -18,4 +18,19 @@ cd ..
 mkdir -p _build/html/es
 rsync -a --delete es/_build/html/ _build/html/es/
 
+# Create index.html to redirect to en/intro.html
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="refresh" content="0; url=en/intro.html">
+    <title>Redirecting...</title>
+</head>
+<body>
+    <p>If you are not redirected, <a href="en/intro.html">click here</a>.</p>
+</body>
+</html>' > index.html
+
+# Copy index.html to the build directory
+cp index.html _build/html/index.html
+
 echo "Both versions built successfully!"
