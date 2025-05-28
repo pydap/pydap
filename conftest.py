ae96d1c96b4bb2dae5d3d2dc5edca7b1b608de3f
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-05-28 14:54:04 -0700

    skip tests related to cmr api unless run locally

--- conftest.py
+++ conftest.py
@@ -1,3 +1,10 @@
+import os
+import pytest
+
+@pytest.fixture(scope='session', autouse=True)
+def set_local_env():
+    os.environ['LOCAL_DEV'] = '1'
+
 # Define which file to ignore in tests:
 collect_ignore = ["setup.py", "bootstrap.py", "docs/conf.py"]
 
