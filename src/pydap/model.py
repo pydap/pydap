2d7f71cb86a8bd15da46505a7a103b5086314f57
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-09-15 23:58:42 -0700

    add safe check dim is a basetype

--- src/pydap/model.py
+++ src/pydap/model.py
@@ -1200,7 +1200,7 @@ class DatasetType(StructureType):
             - slices are not validated, so user must ensure they make sense.
             - Intended only when in batch mode.
         """
-        dims = var.dims
+        dims = [var for var in var.dims if isinstance(self[var], BaseType)]
         var._pending_batch_slice = slice(key) if not key else key
         slice_elements = var.build_ce().split(var.name)[-1]
         dim_slices = dict(zip(dims, [sli + "]" for sli in slice_elements.split("]")]))
