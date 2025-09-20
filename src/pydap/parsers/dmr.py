10559d60f3d048017277be66977a30bbd43f70f6
Miguel Jimenez-Urias <mjimenez@opendap.org>
2025-09-19 18:19:07 -0700

    remove try/exceot

--- src/pydap/parsers/dmr.py
+++ src/pydap/parsers/dmr.py
@@ -266,16 +266,13 @@ def dmr_to_dataset(dmr):
         variable["maps"] = get_maps(variable["element"])
         variable["shape"] = get_dim_sizes(variable["element"])
 
-    try:
-        # Add shape element to variables
-        for name, variable in variables.items():
-            for dim in variable["dims"]:
-                if dim in variables.items():
-                    variable["shape"] += (variables[dim]["size"],)
-                else:
-                    variable["shape"] += (named_dimensions[dim],)
-    except KeyError:
-        print(variables)
+    for name, variable in variables.items():
+        for dim in variable["dims"]:
+            if dim in variables.items():
+                variable["shape"] += (variables[dim]["size"],)
+            else:
+                variable["shape"] += (named_dimensions[dim],)
+
     for name, variable in variables.items():
         var_name = variable["name"]
         path = None
