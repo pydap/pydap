e0f939ea58d403d6714b1bd4cb4bbda6e6d0409a
Miguel Jimenez-Urias <mjimenez@opendap.org>
2026-05-07 13:23:28 -0700

    update DMR generation

--- src/pydap/responses/dmr.py
+++ src/pydap/responses/dmr.py
@@ -7,12 +7,13 @@ clients to introspect the variables and request data as necessary.
 
 """
 
-try:
-    from functools import singledispatch
-except ImportError:
-    from singledispatch import singledispatch
+from collections.abc import Iterable, Mapping
+from functools import singledispatch
+from xml.sax.saxutils import escape
 
-from pydap.lib import __version__
+import numpy as np
+
+from pydap.lib import NUMPY_TO_DAP4_TYPEMAP, __version__
 from pydap.model import BaseType, DatasetType, GroupType, SequenceType, StructureType
 from pydap.responses.lib import BaseResponse
 
@@ -20,8 +21,169 @@ INDENT = " " * 4
 namespace = {"": "http://xml.opendap.org/ns/DAP/4.0#"}
 
 
+def _xml_attr(value):
+    value = escape(str(value), {'"': "&quot;"})
+    return value.encode("ascii", "xmlcharrefreplace").decode("ascii")
+
+
+def _xml_text(value):
+    value = escape(str(_normalize_scalar(value)))
+    return value.encode("ascii", "xmlcharrefreplace").decode("ascii")
+
+
+def _dtype_to_dap4(dtype):
+    dtype = np.dtype(dtype)
+    key = (dtype.kind, dtype.itemsize)
+    if dtype.kind in {"S", "U"}:
+        key = (dtype.kind, None)
+    try:
+        return NUMPY_TO_DAP4_TYPEMAP[key]
+    except KeyError as exc:
+        raise TypeError("Unsupported DAP4 dtype: {dtype}".format(dtype=dtype)) from exc
+
+
+def _normalize_scalar(value):
+    if isinstance(value, np.ndarray) and value.shape == ():
+        value = value.item()
+    elif isinstance(value, np.generic):
+        value = value.item()
+    if isinstance(value, bytes):
+        return value.decode("utf-8")
+    return value
+
+
+def _attribute_values(value):
+    if isinstance(value, np.ndarray):
+        if value.shape == ():
+            return [_normalize_scalar(value)]
+        return [_normalize_scalar(item) for item in value.ravel()]
+    if (
+        isinstance(value, Iterable)
+        and not isinstance(value, (str, bytes, Mapping))
+        and getattr(value, "shape", None) != ()
+    ):
+        return [_normalize_scalar(item) for item in value]
+    return [_normalize_scalar(value)]
+
+
+def _scalar_attribute_type(value):
+    if isinstance(value, np.generic):
+        return _dtype_to_dap4(value.dtype)
+    if isinstance(value, np.ndarray):
+        return _dtype_to_dap4(value.dtype)
+    if isinstance(value, bool):
+        return "UInt8"
+    if isinstance(value, int):
+        return "Int64"
+    if isinstance(value, float):
+        return "Float64"
+    return "String"
+
+
+def _attribute_type(values):
+    types = [_scalar_attribute_type(value) for value in values if value is not None]
+    if not types:
+        return "String"
+    if len(set(types)) == 1:
+        return types[0]
+    if "String" in types:
+        return "String"
+    if any(type_.startswith("Float") for type_ in types):
+        return "Float64"
+    if any(type_.startswith("Int") for type_ in types):
+        return "Int64"
+    if any(type_.startswith("UInt") for type_ in types):
+        return "UInt64"
+    return "String"
+
+
+def _attribute_items(attributes, excluded=()):
+    for key, value in attributes.items():
+        if key not in excluded:
+            yield key, value
+
+
+def _dimensions(attributes):
+    return attributes.get("dimensions", {})
+
+
+def _is_group(child):
+    return isinstance(child, GroupType)
+
+
+def _children_by_declaration_order(var):
+    children = list(var.children())
+    variables = [child for child in children if not _is_group(child)]
+    groups = [child for child in children if _is_group(child)]
+    return variables, groups
+
+
+def _emit_dimensions(dimensions, level):
+    for name, size in dimensions.items():
+        yield '{indent}<Dimension name="{name}" size="{size}"/>\n'.format(
+            indent=level * INDENT, name=_xml_attr(name), size=_xml_attr(size)
+        )
+
+
+def _emit_variable_dimensions(var, level):
+    for dim in var.dims:
+        yield '{indent}<Dim name="{name}"/>\n'.format(
+            indent=level * INDENT, name=_xml_attr(dim)
+        )
+    shape = tuple(var.shape or ())
+    for size in shape[len(var.dims) :]:
+        yield '{indent}<Dim size="{size}"/>\n'.format(
+            indent=level * INDENT, size=_xml_attr(size)
+        )
+
+
+def _emit_attributes(attributes, level, excluded=()):
+    for key, value in _attribute_items(attributes, excluded):
+        yield from _emit_attribute(key, value, level)
+
+
+def _emit_attribute(key, value, level):
+    indent = level * INDENT
+    if isinstance(value, Mapping):
+        yield '{indent}<Attribute name="{name}" type="Container">\n'.format(
+            indent=indent, name=_xml_attr(key)
+        )
+        for child_key, child_value in value.items():
+            yield from _emit_attribute(child_key, child_value, level + 1)
+        yield "{indent}</Attribute>\n".format(indent=indent)
+        return
+
+    values = _attribute_values(value)
+    attribute_type = _attribute_type(values)
+    yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
+        indent=indent, name=_xml_attr(key), type=attribute_type
+    )
+    for item in values:
+        if item is None:
+            yield "{indent}<Value/>\n".format(indent=(level + 1) * INDENT)
+        else:
+            yield "{indent}<Value>{value}</Value>\n".format(
+                indent=(level + 1) * INDENT, value=_xml_text(item)
+            )
+    yield "{indent}</Attribute>\n".format(indent=indent)
+
+
+def _emit_child_variables(var, level):
+    variables, _ = _children_by_declaration_order(var)
+    for child in variables:
+        for line in dmr(child, level):
+            yield line
+
+
+def _emit_child_groups(var, level):
+    _, groups = _children_by_declaration_order(var)
+    for child in groups:
+        for line in dmr(child, level):
+            yield line
+
+
 class DMRResponse(BaseResponse):
-    """The DDS response."""
+    """The DMR response."""
 
     __version__ = __version__
 
@@ -43,113 +205,79 @@ class DMRResponse(BaseResponse):
 
 @singledispatch
 def dmr(var):
-    """Single dispatcher for generating the DODS response."""
+    """Single dispatcher for generating the DMR response."""
     raise StopIteration
 
 
 @dmr.register(DatasetType)
 def _(var, level=0):
     str0 = 'Dataset xmlns="{namespace}"'.format(namespace=namespace[""])
-    str1 = ' xml:base="{url}"'.format(url="http://localhost:8001")
+    str1 = ' xml:base="{url}"'.format(url=_xml_attr("http://localhost:8001"))
     str2 = ' dapVersion="4.0" dmrVersion="1.0"'
-    str3 = ' name="{name}">\n'.format(name=var.name)
+    str3 = ' name="{name}">\n'.format(name=_xml_attr(var.name))
     yield "<{indent}".format(indent=level * INDENT) + str0 + str1 + str2 + str3
-    for name, size in var.dimensions.items():
-        yield '{indent}<Dimension name="{name}" size="{size}"/>\n'.format(
-            indent=(level + 1) * INDENT, name=name, size=size
-        )
-    for child in var.children():
-        for line in dmr(child, level + 1):
-            yield line
-    for key, value in var.attributes.items():
-        if key != "dimensions":
-            _type = type(value).__name__
-            yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
-                indent=(level + 1) * INDENT, name=key, type=_type
-            )
-            yield "{indent}<Value>{val}</Value>\n".format(
-                indent=(level + 2) * INDENT, val=value
-            )
-            yield "{indent}</Attribute>\n".format(indent=(level + 1) * INDENT)
+    yield from _emit_dimensions(_dimensions(var.attributes), level + 1)
+    yield from _emit_child_variables(var, level + 1)
+    yield from _emit_attributes(var.attributes, level + 1, excluded=("dimensions",))
+    yield from _emit_child_groups(var, level + 1)
 
     yield "{indent}</Dataset>\n".format(indent=level * INDENT)
 
 
 @dmr.register(StructureType)
-def _structuretype(var):
-    pass
+def _structuretype(var, level=0):
+    yield '{indent}<Structure name="{name}">\n'.format(
+        indent=level * INDENT, name=_xml_attr(var.name)
+    )
+    yield from _emit_child_variables(var, level + 1)
+    for dim in getattr(var, "dims", ()):
+        yield '{indent}<Dim name="{name}"/>\n'.format(
+            indent=(level + 1) * INDENT, name=_xml_attr(dim)
+        )
+    yield "{indent}</Structure>\n".format(indent=level * INDENT)
 
 
 @dmr.register(SequenceType)
 def _sequencetype(var, level=0, sequence=0):
-    pass
+    yield '{indent}<Sequence name="{name}">\n'.format(
+        indent=level * INDENT, name=_xml_attr(var.name)
+    )
+    yield from _emit_child_variables(var, level + 1)
+    for dim in getattr(var, "dims", ()):
+        yield '{indent}<Dim name="{name}"/>\n'.format(
+            indent=(level + 1) * INDENT, name=_xml_attr(dim)
+        )
+    yield "{indent}</Sequence>\n".format(indent=level * INDENT)
 
 
 @dmr.register(GroupType)
 def _grouptype(var, level=0):
-    yield '{indent}<Group name="{name}">\n'.format(indent=level * INDENT, name=var.name)
-    for dim, size in var.attributes["dimensions"].items():
-        yield '{indent}<Dimension name="{name}" size="{size}"/>\n'.format(
-            indent=(level + 1) * INDENT, name=dim, size=size
-        )
-    for key, value in var.attributes.items():
-        if key not in ["dimensions", "path"]:
-            _type = type(value).__name__
-            yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
-                indent=(level + 1) * INDENT, name=key, type=_type
-            )
-            yield "{indent}<Value>{val}</Value>\n".format(
-                indent=(level + 2) * INDENT, val=value
-            )
-            yield "{indent}</Attribute>\n".format(indent=(level + 1) * INDENT)
-    for child in var.children():
-        for line in dmr(child, level + 1):
-            yield line
+    yield '{indent}<Group name="{name}">\n'.format(
+        indent=level * INDENT, name=_xml_attr(var.name)
+    )
+    yield from _emit_dimensions(_dimensions(var.attributes), level + 1)
+    yield from _emit_child_variables(var, level + 1)
+    yield from _emit_attributes(
+        var.attributes, level + 1, excluded=("dimensions", "path", "Maps")
+    )
+    yield from _emit_child_groups(var, level + 1)
     yield "{indent}</Group>\n".format(indent=level * INDENT)
 
 
 @dmr.register(BaseType)
 def _basetype(var, level=0):
-    _ntype = var.dtype
-    _vartype = str(_ntype)[0].upper() + str(_ntype)[1:]
-    if _vartype == "<U0":
-        _vartype = "String"
+    _vartype = _dtype_to_dap4(var.dtype)
     yield '{indent}<{type} name="{name}">\n'.format(
         indent=level * INDENT,
         type=_vartype,
-        name=var.name,
+        name=_xml_attr(var.name),
     )
-    # get dimensions
-    for dim in var.dims:
-        yield '{indent}<Dim name="{name}"/>\n'.format(
-            indent=(level + 1) * INDENT, name=dim
+    yield from _emit_variable_dimensions(var, level + 1)
+    yield from _emit_attributes(
+        var.attributes, level + 1, excluded=("dims", "Maps", "path")
+    )
+    for _map in var.attributes.get("Maps", ()):
+        yield '{indent}<Map name="{name}"/>\n'.format(
+            indent=(level + 1) * INDENT, name=_xml_attr(_map)
         )
-    for key, value in var.attributes.items():
-        if key != "dims":
-            if isinstance(value, list):
-                _type = type(value[0]).__name__
-                yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
-                    indent=(level + 1) * INDENT, name=key, type=_type
-                )
-                for val in value:
-                    yield "{indent}<Value>{val}</Value>\n".format(
-                        indent=(level + 2) * INDENT, val=val
-                    )
-            else:
-                _type = type(value).__name__
-                yield '{indent}<Attribute name="{name}" type="{type}">\n'.format(
-                    indent=(level + 1) * INDENT, name=key, type=_type
-                )
-                yield "{indent}<Value>{val}</Value>\n".format(
-                    indent=(level + 2) * INDENT, val=value
-                )
-            yield "{indent}</Attribute>\n".format(indent=(level + 1) * INDENT)
     yield "{indent}</{type}>\n".format(indent=level * INDENT, type=_vartype)
-
-    # get maps
-    if "Maps" in var.attributes.keys():
-        Maps = var.Maps
-        for _map in Maps:
-            yield '{indent}<Map name="{name}">\n'.format(
-                indent=(level + 1) * INDENT, name=_map
-            )
--- src/pydap/tests/test_responses_dmr.py
+++ src/pydap/tests/test_responses_dmr.py
@@ -1,15 +1,20 @@
-"""Test the DRM response with DAP4 protocol."""
+"""Test the DMR response with DAP4 protocol."""
 
 import os
 import unittest
+from xml.etree import ElementTree as ET
 
+import numpy as np
+import pytest
 from webob.headers import ResponseHeaders
 from webtest import TestApp as App
 
 from pydap.handlers.lib import BaseHandler
 from pydap.lib import __version__
-from pydap.responses.dmr import dmr
-from pydap.tests.datasets import SimpleGroup
+from pydap.model import BaseType, DatasetType
+from pydap.parsers.dmr import dmr_to_dataset
+from pydap.responses.dmr import DMRResponse, dmr
+from pydap.tests.datasets import SimpleArray, SimpleGroup
 
 
 def load_dmr_file(file_path):
@@ -20,7 +25,7 @@ def load_dmr_file(file_path):
 
 
 class TestDMRResponseGroup(unittest.TestCase):
-    """Test DRM response from sequences."""
+    """Test DMR response from groups."""
 
     def setUp(self):
         """Create a simple WSGI app."""
@@ -58,7 +63,7 @@ class TestDMRResponseGroup(unittest.TestCase):
                         "Access-Control-Allow-Headers",
                         "Origin, X-Requested-With, Content-Type",
                     ),
-                    ("Content-Length", "2276"),
+                    ("Content-Length", "2302"),
                 ]
             ),
         )
@@ -67,3 +72,91 @@ class TestDMRResponseGroup(unittest.TestCase):
         """Test the generated DMR response"""
         dmr_text = load_dmr_file("data/dmrs/SimpleGroup.dmr")
         self.assertEqual(self.res.text, dmr_text)
+
+    def test_body_parses_back_to_dataset(self):
+        """Generated grouped DMR can be parsed back."""
+        dataset = dmr_to_dataset(self.res.text)
+        self.assertEqual(dataset["/SimpleGroup/Temperature"].dtype, np.dtype("f4"))
+        self.assertEqual(
+            dataset["/SimpleGroup/Temperature"].dims,
+            SimpleGroup["/SimpleGroup/Temperature"].dims,
+        )
+
+
+class TestDMRResponseArray(unittest.TestCase):
+    """Test DMR response for simple arrays."""
+
+    def setUp(self):
+        app = App(BaseHandler(SimpleArray))
+        self.res = app.get("/.dmr")
+
+    def test_body(self):
+        dmr_text = load_dmr_file("data/dmrs/SimpleArray.dmr")
+        self.assertEqual(self.res.text, dmr_text)
+
+    def test_body_parses_back_to_dataset(self):
+        dataset = dmr_to_dataset(self.res.text)
+        self.assertEqual(dataset["byte"].dtype, np.dtype("u1"))
+        self.assertEqual(dataset["byte"].shape, (5,))
+        self.assertEqual(dataset["string"].dtype, np.dtype("|S128"))
+        self.assertEqual(dataset["string"].shape, (2,))
+
+
+def test_dmr_escapes_xml_names_and_attribute_values():
+    dataset = DatasetType('quoted "dataset"')
+    dataset['quoted"name'] = BaseType(
+        'quoted"name',
+        data=np.array([1], dtype="i4"),
+        attributes={"summary": 'A & B < C > D "quoted"'},
+    )
+
+    body = b"".join(DMRResponse(dataset)).decode("ascii")
+
+    ET.fromstring(body)
+    assert 'name="quoted%20&quot;dataset&quot;"' in body
+    assert 'name="quoted&quot;name"' in body
+    assert 'A &amp; B &lt; C &gt; D "quoted"' in body
+
+
+@pytest.mark.parametrize(
+    "dtype, value, expected_type",
+    [
+        ("?", True, "UInt8"),
+        ("i1", 1, "Int8"),
+        ("u1", 1, "UInt8"),
+        ("i2", 1, "Int16"),
+        ("u2", 1, "UInt16"),
+        ("i4", 1, "Int32"),
+        ("u4", 1, "UInt32"),
+        ("i8", 1, "Int64"),
+        ("u8", 1, "UInt64"),
+        ("f4", 1.0, "Float32"),
+        ("f8", 1.0, "Float64"),
+        ("S1", b"x", "String"),
+        ("U1", "x", "String"),
+    ],
+)
+def test_dmr_uses_explicit_dap4_type_map(dtype, value, expected_type):
+    dataset = DatasetType("types")
+    dataset["value"] = BaseType("value", data=np.array([value], dtype=dtype))
+
+    body = b"".join(DMRResponse(dataset)).decode("ascii")
+
+    assert '<{type} name="value">'.format(type=expected_type) in body
+
+
+def test_dmr_emits_nested_groups_after_sibling_variables():
+    dataset = dmr_to_dataset(load_dmr_file("data/dmrs/Nested_Group.dmr"))
+
+    body = b"".join(DMRResponse(dataset)).decode("ascii")
+
+    ET.fromstring(body)
+    assert body.index('<Float64 name="root_variable">') < body.index(
+        '<Group name="Group1">'
+    )
+    assert body.index('<Float64 name="group_1_var">') < body.index(
+        '<Group name="subgroup1">'
+    )
+    assert '<Float64 name="subgroup1_var">' in body
+    assert '<Attribute name="path"' not in body
+    assert '<Attribute name="Maps"' not in body
