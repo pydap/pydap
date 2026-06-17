107d146fd9ffb8945cd8aed2bd9577eeb7a4f12f
Miguel Jimenez <mjimenez@opendap.org>
2026-06-17 12:01:56 -0700

    add warning about expat version if < 2.4.0 (#699)
    * add warning about expat version if < 2.4.0
    
    * add a test for expat_version

--- src/pydap/__init__.py
+++ src/pydap/__init__.py
@@ -1,3 +1,16 @@
+import pyexpat
+
 from ._version import __version__
+
+if pyexpat.version_info < (2, 4, 0):
+    import warnings
+
+    warnings.warn(
+        f"expat {pyexpat.EXPAT_VERSION} is vulnerable to DoS attacks (CVE-2022-23852). "
+        "Upgrade your Python installation or system expat library to expat >= 2.4.0.",
+        RuntimeWarning,
+        stacklevel=2,
+    )
+
 
 __all__ = ["__version__"]
