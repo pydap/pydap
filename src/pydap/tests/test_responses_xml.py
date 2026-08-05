d00a40d17443501dd05535a066fa6b29fb79518a
Miguel Jimenez-Urias <mjimenez@opendap.org>
2026-08-05 15:44:18 -0700

    tests with phony_dims

--- src/pydap/tests/test_responses_xml.py
+++ src/pydap/tests/test_responses_xml.py
@@ -8,12 +8,13 @@ from pydap.responses.xml import (
     _basetype_element,
     _group_element,
     _new_root,
+    _qual_name,
     _sequence_element,
     build_dmr_element,
     build_dmr_tree,
     dmr_tree_to_string,
 )
-from pydap.tests.datasets import SimpleGroup, SimpleSequence
+from pydap.tests.datasets import DSUnDims, SimpleGroup, SimpleSequence
 
 
 # the following should work but currently fails
@@ -88,6 +89,15 @@ def test_build_dmr_element_sequence():
     assert element.tag == "{" + DAP4_NS["dap"] + "}" + "Sequence"
     assert element.get("name") == "cast"
     assert element.findall("dap:String", DAP4_NS)[0].get("name") == "id"
+    assert [el.get("name") for el in element.findall("dap:Int64", DAP4_NS)] == [
+        "lon",
+        "lat",
+        "depth",
+        "time",
+        "temperature",
+        "salinity",
+        "pressure",
+    ]
 
 
 def test_roundtrip_dataset_xml_dataset():
@@ -98,3 +108,41 @@ def test_roundtrip_dataset_xml_dataset():
 
     assert dataset.variables() == SimpleGroup.variables()
     assert SimpleGroup["SimpleGroup"].variables() == dataset["SimpleGroup"].variables()
+
+
+def test_qual_name():
+    """Tests that the DAP4 namespace is added to the tag of an element."""
+    assert _qual_name("Dataset") == "{" + DAP4_NS["dap"] + "}" + "Dataset"
+
+
+def test_roundtrip_dataset_xml_dataset_with_unnamed_dimensions():
+    """Test that a dataset with unnamed dimensions can be converted to xml
+    and back to a dataset."""
+    domtree = build_dmr_tree(DSUnDims)
+    dmr = dmr_tree_to_string(domtree)
+    dataset = dmr_to_dataset(dmr)
+
+    assert list(dataset["Group1"].variables()) == list(DSUnDims["Group1"].variables())
+    assert list(DSUnDims["Group2"].variables()) == list(dataset["Group2"].variables())
+
+    assert dataset["Group1"].dimensions == {
+        "phony_dim_0": 1,
+        "phony_dim_1": 4,
+        "phony_dim_2": 4,
+    }
+    assert dataset["Group2"].dimensions == {
+        "phony_dim_0": 1,
+        "phony_dim_1": 4,
+        "phony_dim_2": 4,
+    }
+
+    assert dataset["Group1/Temperature"].dims == [
+        "/Group1/phony_dim_0",
+        "/Group1/phony_dim_1",
+        "/Group1/phony_dim_2",
+    ]
+    assert dataset["Group2/Salinity"].dims == [
+        "/Group2/phony_dim_0",
+        "/Group2/phony_dim_1",
+        "/Group2/phony_dim_2",
+    ]
